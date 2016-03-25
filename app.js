32418927111ea5f9ddb815d82b34f3af5715e820
Josh Richard <eos.josh.richard@gmail.com>
2016-03-25 19:17:51 -0400

    first commit of the new repo

---
+++ app.js
@@ -0,0 +1,33 @@
+/* eslint-env node */
+var express = require('express');
+var fs = require('fs');
+var path = require('path');
+var nconf = require('nconf');
+
+var mongo = require('./data/mongo.js');
+var password = require('./data/password.js');
+
+var app = express();
+
+nconf.file({
+    file: './config.json'
+});
+
+var port = nconf.get('app:port');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res) {
+    res.redirect('index.html');
+});
+
+var childCollection = nconf.get('mongo:childCollection');
+var donorCollection = nconf.get('mongo:donorCollection');
+var cartCollection = nconf.get('mongo:cartCollection');
+
+app.listen(port, function () {
+    console.log('Express port listening at ' + port);
+});
